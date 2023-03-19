24595d0b3ef15290e532166b0412daee771b303a
Blackdoll-Loki <helpmeloki7@gmail.com>
2023-03-19 23:45:37 +0200

    feat: add styles to Search and save input in LocalStorage

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../styles/Search.css';
 
 class Search extends React.Component {
   render() {
+    function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+      const storage = window.localStorage;
+      storage.setItem('input', event.target.value);
+    }
     return (
       <div className="Search-block">
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Search" onChange={handleInput} />
       </div>
     );
   }
